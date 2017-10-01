f8d47419542c1373ca830c7663bd7410f4eb63ce
Chris Callan <chriscallan@comcast.net>
2017-10-01 05:45:12 -0700

    Angular Components Part 1:     - Added images to the project;     - updated the template to add a menu;     - created a menu component to add to the page;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
 import 'hammerjs';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, 
