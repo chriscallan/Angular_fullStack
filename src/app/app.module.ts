9c28646d643945edeb9fad86364ca8be6df48c2d
Chris Callan <chriscallan@comcast.net>
2017-10-06 13:08:38 -0700

    Week 2: Assignment 2 -     - Created a Leader service with supporting data;     - Updated App module to bring this service in;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -39,7 +40,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatGridListModule, 
     MatCardModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
