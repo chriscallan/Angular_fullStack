854bbf2d081ff4a172b3b585dfed306e69ccdc2c
Chris Callan <chriscallan@comcast.net>
2017-10-10 17:03:35 -0700

    Week 3: Angular and RxJs -      - setup the services to make use of Observables classes;     - updated the consumers of the services to use Observalbe patter;

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -18,7 +18,7 @@ export class AboutComponent implements OnInit {
     private location: Location) { }
 
   ngOnInit() {
-    this.leaderservice.getLeaders().then(leaders => this.leaders = leaders);
+    this.leaderservice.getLeaders().subscribe(leaders => this.leaders = leaders);
   }
 
   goBack(): void {
