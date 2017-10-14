83b5a383d0bf1eb784925be5251be368f4745d16
Chris Callan <chriscallan@comcast.net>
2017-10-14 06:23:58 -0700

    Week 4: Angular Animations Part 1 -     - Created some animations and applied them to the dishdetail component;

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations'; 
 
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/catch'; 
@@ -12,7 +13,20 @@ import 'rxjs/add/operator/catch';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],   
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 export class DishdetailComponent implements OnInit {
@@ -29,6 +43,7 @@ export class DishdetailComponent implements OnInit {
     'author': '', 
     'comment': ''
   }
+  visibility = 'shown'; 
 
   validationMessages = {
     'author': {
@@ -52,8 +67,12 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {    
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); });
+      .switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); })
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+          errmess => { this.dish = null; this.errMess = <any>errmess; });
+    // this.route.params
+    //   .switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
+    //   .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); });
   }
 
   createForm(): void {
