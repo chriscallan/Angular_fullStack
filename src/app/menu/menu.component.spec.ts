e4780fa884b0a0c709cffd9d3d07c0169541e78d
Chris Callan <chriscallan@comcast.net>
2017-10-15 06:47:56 -0700

    Week 4: Testing Angular Applications - (Component Test) -     - POS lesson that is more broken than Muppala's code usually is;     - Doesn't work on my box and will take way too much effort to fix for an optional lesson (moving on);

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu.component';
+import { MdToolbarModule, MdButtonModule, MdCheckboxModule, MatListModule, MatGridListModule, MatCardModule } from '@angular/material';
+import { MdDialog, MdDialogRef, MdDialogModule, MdInputModule, MdOption, MdSelectModule, MdFormFieldModule } from '@angular/material';
+import { MdGridTile, MatGridList, MdSpinner, MatGridTile } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout'; 
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import {baseURL } from '../shared/baseurl';
+import { Observable } from 'rxjs/Observable';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+    let dishServiceStub = {
+      getDishes: function(): Observable<Dish[]>{
+        return Observable.of(DISHES); 
+      }
+    }
+    
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [ BrowserAnimationsModule, 
+        FlexLayoutModule, 
+      RouterTestingModule.withRoutes(
+        [{path: 'menu', component: MenuComponent}]
+      )], 
+      declarations: [ MenuComponent ], 
+      providers: [
+        { provide: DishService, useValue: dishServiceStub }, 
+        {provide: 'BaseURL', useValue: baseURL }
+      ]
     })
     .compileComponents();
+
+    let dishService = TestBed.get(DishService); 
   }));
 
   beforeEach(() => {
@@ -21,5 +50,17 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+    
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
   });
 });
